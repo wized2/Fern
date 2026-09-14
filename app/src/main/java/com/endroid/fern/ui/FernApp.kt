24d38eaf79d9906bca884f308b06c55eb44930f7
wized2 <code@canvas.app>
2026-09-14 12:14:08 +0000

    fix: import androidx.compose.ui.Modifier.Modifier

--- app/src/main/java/com/endroid/fern/ui/FernApp.kt
+++ app/src/main/java/com/endroid/fern/ui/FernApp.kt
@@ -50,7 +50,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.Modifier.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
