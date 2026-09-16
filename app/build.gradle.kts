33e2678a778fe10b94cacb6ed4c981f1a142893b
wized2 <code@canvas.app>
2026-09-16 02:15:02 +0000

    chore: widget update every 15m + v1.5.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.endroid.fern"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.5.7"
+        versionCode = 20
+        versionName = "1.5.8"
         resourceConfigurations += listOf("en")
         buildConfigField("String", "VERSION_NAME", "\"1.5.1\"")
     }
