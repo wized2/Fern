bd064d5b525d9b255ec22f2de214c23eafab4658
Mudsir Wazir <wa0026343@gmail.com>
2026-09-18 15:14:01 +0500

    chore: Fern 1.5.27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.endroid.fern"
         minSdk = 26
         targetSdk = 35
-        versionCode = 38
-        versionName = "1.5.26"
+        versionCode = 39
+        versionName = "1.5.27"
         resourceConfigurations += listOf("en")
         buildConfigField("String", "VERSION_NAME", "\"1.5.19\"")
     }
