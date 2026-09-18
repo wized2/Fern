473042bdfdd61e82b38c04b96594d8c6abae3739
Mudsir Wazir <wa0026343@gmail.com>
2026-09-18 06:51:24 +0500

    chore: version 1.5.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.endroid.fern"
         minSdk = 26
         targetSdk = 35
-        versionCode = 34
-        versionName = "1.5.22"
+        versionCode = 36
+        versionName = "1.5.24"
         resourceConfigurations += listOf("en")
         buildConfigField("String", "VERSION_NAME", "\"1.5.19\"")
     }
